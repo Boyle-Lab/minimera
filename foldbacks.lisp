de05a662a19adf6240f662eec0c07646a357f412
Steve Losh <steve@stevelosh.com>
2025-06-18 10:46:15 -0400

    Progress on clustering, etc

--- foldbacks.lisp
+++ foldbacks.lisp
@@ -35,9 +35,10 @@
          (foldback-length (- n real-length))
          (prefix (iterate (repeat real-length)
                           (collect (random-elt "ACTG") :result-type 'string)))
-         (suffix (reverse-complement (subseq prefix (- (length prefix) foldback-length)))))
+         (suffix (reverse-complement (subseq prefix (- (length prefix) foldback-length))))
+         (adapter (random-dna (random-range 80 100))))
     ;; (format t "~v@A~%~v@A" real-length prefix real-length (reverse suffix))
-    (values (concatenate 'string prefix suffix)
+    (values (concatenate 'string prefix adapter suffix)
             (format nil "~A foldback ~D" (uuid:make-v4-uuid) foldback-length)
             foldback-length)))
 
@@ -70,8 +71,74 @@
   (values))
 
 
-;;;; Reference Implementation -------------------------------------------------
+;;;; Longest Increasing Subsequence -------------------------------------------
+;;; Based on Rosetta code, cleaned up and extended to allow :key function:
+;;; https://rosettacode.org/wiki/Longest_increasing_subsequence#Common_Lisp:_Using_the_Patience_Sort_approach
 
+(defun patience-insert (item piles &key key)
+  ;; piles is an array that will be mutated.  each pile is ((item . backpointer)
+  ;; …) where backpointer points at the previous element (and this conveniently
+  ;; happens to resemble vanilla list structure).
+  (loop :for pile :across piles
+        :and prev = nil :then pile
+        :for i from 0
+        :for top = (car pile)
+        :do (when (<= (funcall key item)
+                      (funcall key (car top)))
+              (push (cons item (car prev)) (aref piles i))
+              (return))
+        :finally (vector-push-extend (list (cons item top)) piles)))
+
+(defun longest-increasing-subsequence (sequence &key (key #'identity))
+  (let ((piles (make-array 256 :adjustable t :fill-pointer 0)))
+    (map nil (lambda (item) (patience-insert item piles :key key)) sequence)
+    (nreverse (car (aref piles (1- (length piles)))))))
+
+
+;;;; Reference Implementation -------------------------------------------------
+(deftype u64 ()
+  `(integer 0 ,(1- (expt 2 64))))
+
+(defun-inline wrap64 (n)
+  (mod n (expt 2 64)))
+
+(defun-inline shl (x bits)
+  (logand (ash x bits)
+          (1- (ash 1 64))))
+
+(defun-inline shr (x bits)
+  (logand (ash x (- bits))
+          (1- (ash 1 64))))
+
+(defun invertible-hash (n)
+  (etypecase n
+    (u64
+      (_ n
+        (wrap64 (+ (lognot _) (shl _ 21)))
+        (wrap64 (logxor _ (shr _ 24)))
+        (wrap64 (+ (+ _ (shl _ 3))
+                   (shl _ 8)))
+        (wrap64 (logxor _ (shr _ 14)))
+        (wrap64 (+ (+ _ (shl _ 2))
+                   (shl _ 4)))
+        (wrap64 (logxor _ (shr _ 28)))
+        (wrap64 (+ _ (shl _ 31)))))))
+
+;; (format t "~16,'0X" (invertible-hash #x7ffffbffffdfffff))
+
+
+(defun minhash (string &key (start 0) (end (length string)))
+  (iterate
+    (with hash = 0)
+    (for ch :in-string string :from start :below end)
+    (zapf hash (_ %
+                 (ash _ 2)
+                 (logior _ (ecase ch
+                             (#\A #b00)
+                             (#\C #b01)
+                             (#\T #b10)
+                             (#\G #b11)))))
+    (returning (invertible-hash hash))))
 
 
 (defun do-fastq (function stream)
@@ -105,8 +172,14 @@
     (for minimizer = (alexandria:extremum
                        (iterate
                          (for (kmer ks ke) :window k :on window)
-                         (collect (cons kmer (+ ws ks))))
-                       #'string-minimizer-<))
+                         (collect
+                           (cons (minhash kmer) (+ ws ks))
+                           ;; (cons kmer (+ ws ks))
+
+                           ))
+                       #'< :key #'car
+                       ;; #'string-minimizer-<
+                       ))
     (unless (and prev (= (cdr prev) (cdr minimizer)))
       (collect minimizer)
       (setf prev minimizer))))
@@ -128,13 +201,48 @@
 (defun hits (ms1 ms2)
   (let ((index-1 (group-by #'car ms1 :test #'equal :map #'cdr))
         (index-2 (group-by #'car ms2 :test #'equal :map #'cdr)))
-    (sort (iterate
-            (for (minimizer locs-1) :in-hashtable index-1)
-            (for locs-2 = (gethash minimizer index-2))
-            (appending (alexandria:map-product (lambda (l1 l2)
-                                                 (make-hit (- l2 l1) l1 l2))
-                                               locs-1 locs-2)))
+    (sort (coerce (iterate
+                    (for (minimizer locs-1) :in-hashtable index-1)
+                    (for locs-2 = (gethash minimizer index-2))
+                    (appending (alexandria:map-product (lambda (l1 l2)
+                                                         (make-hit (- l2 l1) l1 l2))
+                                                       locs-1 locs-2)))
+                  'vector)
           #'hit<)))
+
+(define-sorting-predicate cluster-hit-<
+  (#'< :key #'hit-l1)
+  (#'< :key #'hit-l2))
+
+(define-sorting-predicate cluster-hit->
+  (#'> :key (lambda (hit) (+ (* (hit-l1 hit) (hit-l1 hit))
+                             (* (hit-l2 hit) (hit-l2 hit)))))
+  (#'> :key #'hit-l1)
+  (#'> :key #'hit-l2))
+
+(defun cluster (hits &key (epsilon 10) (min-length 10))
+  (unless (zerop (length hits))
+    (iterate
+      (with last-i = (1- (length hits)))
+      (with start = 0)
+      (for hit :in-vector hits :with-index i)
+      (for next-i = (1+ i))
+      (for next-hit = (if (= i last-i)
+                          nil
+                          (aref hits next-i)))
+      (when (or (null next-hit)
+                (> (- (hit-c next-hit) (hit-c hit))
+                   epsilon))
+        (when (>= (- next-i start) min-length)
+          (collect (subseq hits start next-i) :into results))
+        (setf start next-i))
+      (finally
+        (prl (mapcar #'length results))
+        (let ((deduped (mapcar (lambda (cluster)
+                                 (longest-increasing-subsequence (sort cluster #'< :key #'hit-l1) :key #'hit-l2))
+                               results)))
+          (prl (mapcar #'length deduped))
+          (return deduped))))))
 
 (defun foldback-score (seq &key (k 4) (w 12))
   (let* ((minimizers-id (minimizers k w seq))
@@ -143,40 +251,64 @@
     (prl hits))
   0.0d0)
 
-(defun run (filename)
-  (with-open-file (f filename :direction :input)
-    (gathering
-      (do-fastq (lambda (id seq)
-                      (identity (cons id (foldback-score seq))))
-                    f))))
+;; (defun run (filename)
+;;   (with-open-file (f filename :direction :input)
+;;     (gathering
+;;       (do-fastq (lambda (id seq)
+;;                       (identity (cons id (foldback-score seq))))
+;;                     f))))
+
+
+
+(defparameter *data/okay-1*     (alexandria:read-file-into-string "data/okay1"))
+(defparameter *data/foldback-1* (alexandria:read-file-into-string "data/foldback1"))
+(defparameter *data/foldback-2* (alexandria:read-file-into-string "data/foldback2"))
+(defparameter *data/foldback-3* (alexandria:read-file-into-string "data/foldback2"))
+
+;; (write-random-fastq "test.fastq" 100 200 400)
+
+(defun index-hits (clusters)
+  (iterate
+    (for i :from 1)
+    (for hits :in clusters)
+    (format t "cluster ~D: ~D hits~%" i (length hits))
+    (doseq (hit hits)
+      (collect-hash (hit i)))))
+
+(defun run-test (sequence &key k w (mut? t) (cluster-epsilon 10) (cluster-min 10))
+  (let* ((seq (if mut? (mutate-dna sequence) sequence))
+         (rlen (length seq)))
+    (with-open-file (f "hits.csv" :direction :output :if-exists :supersede)
+      (conserve:write-row (list "c" "l1" "l2" "cluster") f)
+      (let* ((hits (hits (minimizers k w seq)
+                         (minimizers k w (reverse-complement seq))))
+             (clusters (index-hits (cluster hits :epsilon cluster-epsilon :min-length cluster-min))))
+        (doseq (hit hits)
+          (conserve:write-row (list (princ-to-string (hit-c hit))
+                                    (princ-to-string (hit-l1 hit))
+                                    (princ-to-string (hit-l2 hit))
+                                    (princ-to-string (gethash hit clusters "NA")))
+                              f))))
+    (write-line "Plotting…")
+    (rscript-file "plot.R" rlen)))
 
 
 #; Scratch --------------------------------------------------------------------
 
-(write-random-fastq "test.fastq" 100 200 400)
+(defparameter *seq* (mutate-dna (random-foldback 20000)))
 
-(multiple-value-bind (seq id l) (random-foldback 100000)
-  (declare (ignore l))
-  (let* ((seq "AGTCCCCAGTTAAACACCAGGTTTGGGAATGCTGCGGCAGGAGGATCGCTTGAGCCCATGAGTTTGAGACCAGCCAGAACAATATAGTGAGACCCTGTCTCTAAAAAAAAAAAAAAAAAAAAAAAAATTTAAACACTTAGCTGAGGCATGGTGGGGCATGCCTGTAGTCCCAGCTACACTGGGAGGCTGTGGTAGGAGGGTCGTTTGAGCTTGGAATATTGAGGCTGTAGTGAATGGTGATCAAGCCACTGCACTCCAGCCTGGGTAACAGAGGGAGACTCTGTCTCATAAATCAAACGTTTTGTATAGATTCCCATAGAAGTGAGTTAGGCATCAATCATAGAGTTATTAGCCACTTTGGTGTCTACCTTGGGAGTAAAACATATAATAAGGGGCAGCTTTAAACCATCTCAATCAATAACCTCCAACTTCTCGAGAAGGTTCTTATTTCATGAATTTCTAAACAAGAGACTACCTAGATTAAGACATTTGGTGGACACCATTTTGAGATGAAGAATCTGGAGTGGGAAGAAGGGAGATCTCTACTTACTGAAGCTTCCCAATGACATAGTTAAGTGTTCCCCAAAAGAAACTTTAGAACAAGAATTTCATCGTGCCATATCTCTATGAAAAGGAATTTCTCTAAAAGAAAACAAAGGCAAACAAGTGATAATCTGATTCTCATGGGAAAGTTTTCATCATAAAAGAAAAAGAGTGCTGGTTGCCGTGGCTCACGTCTGTAATCCCAACAGTTTGGGAGGCTGAGGTGGGTGGATTACCTGAGGTCAGAAGTTCAAAAACAGCCTGGCCAACATGGTGAAACCCTGTCTCTACTGAAAATACAAAAATTAGCCAGGTGTGGTGGTGTGCACCTGTAGTCCCAGCTACTTGGGAGGCCGAGGCAGGAGAATCACTTGAACCCAGGAGGTGGGAGTTGCAGTAAGCCGAGATGGTGCCACTGCACTCCAGGCTAGATGACACAGTGTGACTCCATCTCAAAAAAAAGAAAAAAAGAAAAACAAAAAAGGGACAAAGTATACTGGTCCAAAAAAGAAGAAGGCAAGAAAAAAAGGACAAAGTATATTGTTTACTATCATCACAGTGAGATAGTCCCCCTTTGAGATTAGAAAATAACAGTATACTCAAAGTAACATTAATGAGAACCAACATAAAATAGACAACATTCACTATCTACAAAAGTAATCTGCACCAATTAGCAATGTATGAGCATGTGGTTGAGAATATTGTCTATAATATGTATGTTAGAAGGAAGAGACCTCAAGAAAATGGTCAGAGCTGGAAATGTAGATTAGGGAATCTAGGTCAAAGTTTTGAGATTTTAGGAGTCCTGAGAGAATTTAAAAAGAGAAATAGCCACCAGGCGTGGTGGCCACACCTGTAATCCCAGCACTTTGGGAGGCCAAGGCAGGAAGATCATGAGGTCAGGAGTTCAAGACCAGTCTGGCCAACATAGTGAAACCCCGTCTCTACTAAAAATACAAAAAATTAGCTGGGCTTGGTAACACATACCTGTAATCCTAGCTACTTGGGAGGCTGAGGCAGGAGAATCGCTTGAACACACGAGGTGGAGGTTGCATTGAGCTGAGATCATGCCACTGCACTCCAGACTGGGTGACAGTGGGAGACTCCATCTCAAAACAAAAAACGAAAACAAAACAAAACAAAAAAACAGAAAAGGATAGGACTAAAGAACAGAGGTTGCTAAATTTAGAAATGAAGCAGGATCAGAGGAATAGGAAGGGATAGGGTAAAGAACAGATGTTGCTGCATTTAGAAAGGAAGGGGGTCAGAGGAATAGAAAGGGATAGGGCTGAAGAATAGAGGTCGCTGCATTTATAAATGAAGCCGAGTCAGGAATAGAAAGAGATAGGGCTGAAGAACAGAGGTTGCTGCATTTAGAAATGAAGTAGGGTCAGAGGAATAGAAAGGGATAGGGCTGAAGAACAGAGGTCACTGCATTTAGAAAGGAAGCGGGGTCAGAGGAGCAGAGGGAGCATTTGGTCACTGCTCTGCTGAGCAAAGCAGGATAAAGTCCTCCATGACCCTTGGACTTCTATATTGGAATTATTAAAAATCAGATTTCAGAATAAAAAACACAATAAGTGATGAAAAATGGATTTCTGAATGAGATCATGTGTCATAGAGTCCAATGGAAGGGGAGAAACAGGATATTAGAAAAGCCACAAAAAGTAGACAAAAGTTGTTTTTGTTGATTATAGA")
-         (k 4)
-         (w 12)
-         (mut (mutate-dna seq)))
-    (prl seq id)
-    ;; (print-foldback seq l)
-    (flet ((run (seq filename)
-             (with-open-file (f filename :direction :output :if-exists :supersede)
-               (conserve:write-row (list "c" "l1" "l2") f)
-               (dolist (hit (hits (minimizers k w seq)
-                                  (minimizers k w (reverse-complement seq))))
-                 (conserve:write-row (list (princ-to-string (hit-c hit))
-                                           (princ-to-string (hit-l1 hit))
-                                           (princ-to-string (hit-l2 hit)))
-                                     f)))
+(defparameter *seq* (random-foldback 20000))
 
-             ))
-      (run seq "hits-seq.csv")
-      (run mut "hits-mut.csv"))))
+(defparameter *seq* (random-dna 20000))
+
+(run-test *seq* :k 12 :w 15 :mut? nil :cluster-epsilon 30 :cluster-min 10)
 
 
+(run-test (alexandria:read-file-into-string "data/foldback4")
+          :k 8 :w 20
+          :mut? nil
+          :cluster-epsilon 30 :cluster-min 20)
 
-(run "test.fastq")
+(run-test (alexandria:read-file-into-string "data/ligation-with-rep") :k 12 :w 25 :mut? nil)
+
+(run-test (pbpaste) :k 12 :w 20 :mut? nil)
--- plot.R
+++ plot.R
@@ -1,29 +1,65 @@
-library(tidyverse)
+library(tidyverse, quietly=TRUE)
+
+args <- commandArgs(trailingOnly=TRUE)
+rlen <- as.numeric(args[1])
 
 # data <- read_csv("hits-seq.csv")
-data <- read_csv("hits-mut.csv")
+# data <- read_csv("hits-mut.csv")
+data <- read_csv(
+  "hits.csv",
+  col_types = list(col_integer(), col_integer(), col_integer(), col_factor())
+)
 
 matches <- data |>
   rename(x=l1, y=l2)
 
+ymax <- max(matches |> pull(y))
+xmax <- max(matches |> pull(x))
+ymax <- max(c(ymax, xmax))
+
 intercepts <- data |>
   select(c) |>
-  rename(y=c) |>
+  mutate(h=sqrt(2 * ((ymax - abs(c))**2))) |>
+  mutate(y=c) |>
   mutate(x=0)
 
+intercepts |>
+  summarize(cmean=mean(c), csd=sd(c))
+
+intercepts
+
+tic <- 2000
+
+xbreaks <- seq(0, rlen, by=tic)
+ybreaks <- c(rev(-seq(0, rlen, by=tic)), seq(tic, rlen, by=tic))
 
 ggplot(matches, aes(x=x, y=y)) +
-  geom_point(shape='o') +
-  geom_jitter(color="#FF000066", shape='o', data=intercepts, width=5, height=1) +
+  coord_fixed(xlim=c(0, rlen),
+              ylim=c(-rlen, rlen),
+              ratio=1) +
+  scale_x_continuous(breaks=xbreaks) +
+  scale_y_continuous(breaks=ybreaks) +
+  geom_point(shape='o', aes(color=cluster)) +
+  geom_segment(x=0,    y=0,    xend=0,    yend=rlen, color="#666666") +
+  geom_segment(x=rlen, y=0,    xend=rlen, yend=rlen, color="#666666") +
+  geom_segment(x=0,    y=0,    xend=rlen, yend=0, color="#666666") +
+  geom_segment(x=0,    y=rlen, xend=rlen, yend=rlen, color="#666666") +
+  geom_segment(x=0,    y=0,    xend=rlen, yend=rlen, color="#AA6666", linetype="dashed") +
+  geom_jitter(color="#00000066", shape='o', data=intercepts) +
   labs(
     title="Mimimizer matches",
     x="Location in Read 1",
     y="Location in Read 2",
   )
 
+ggsave("matches.png", width=6, height=8, unit="in", dpi=600)
 
-# ggplot(intercepts, aes(x=y)) +
-#   geom_histogram() +
-#   labs(
-#     title="y-intercepts of matches",
-#   )
+
+ggplot(intercepts, aes(x=y)) +
+  # geom_histogram(bins=100, aes(y=after_stat(count / pmax(20000, sqrt(2 * ((ymax - abs(x))**2)))))) +
+  geom_histogram(bins=30) +
+  labs(
+    title="y-intercepts of matches",
+  ) + geom_line(aes(y=h/100000000))
+
+ggsave("intercepts.png")
