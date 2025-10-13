ba80956d8494fa2ba572d6cacff01ea7d3cc18ba
Steve Losh <steve@stevelosh.com>
2025-10-13 12:14:47 -0400

    Update clipboard script

--- contrib/minimera-clipboard.sh
+++ contrib/minimera-clipboard.sh
@@ -1,27 +1,49 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eu
 
-function fastq {
+set -x
+function seq_to_fastq {
     echo "@scratch"
-    pbpaste | head -n1
+    pbpaste | tr -d '\n' | head -n1
     echo
     echo "+"
-    pbpaste | head -n1 | sed -Ee 's/./I/g'
+    pbpaste | tr -d '\n' | head -n1 | sed -Ee 's/./I/g'
     echo
 }
 
+function clean_fastq {
+    pbpaste | awk '
+    NR <= 4 {
+        printf("%s\n", $0)
+    }
+    '
+}
+
+function read_id {
+    pbpaste | head -n1 | cut -f1 | tail -c +2
+}
 
 scratch="/home/sjl/scratch/minimera-clipboard-scratch"
 mkdir -p "$scratch"
 
-if test -f "$scratch"/plots/scratch.png; then
-    rm "$scratch"/plots/scratch.png
-fi
+# if test -f "$scratch"/plots/scratch.png; then
+#     rm "$scratch"/plots/scratch.png
+# fi
 
-minimera <(fastq) \
+id=$(read_id)
+# fastq
+
+minimera <(clean_fastq) \
     --output "$scratch" \
     --plot-foldbacks \
     --plot-normal
 
-qimgv "$scratch"/plots/scratch.png
+cp  "$scratch"/plots/"$id".png \
+    "$scratch"/plots/scratch.png
+
+if ps -ax | grep -P '[q]imgv' > /dev/null; then
+    echo qimgv already running
+else
+    nohup qimgv "$scratch"/plots/scratch.png >/dev/null &
+fi
